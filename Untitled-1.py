81673cc00d350b8b9554ecd9f79f2bdde20a3f0b
fd9fd <fhido355@gmail.com>
2023-11-29 20:50:50 +0300

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+def main():
+    number = RandomNumber()
+    guess = input("Enter a number")
+    CheckUserChoice(number, guess)
+    return None
+
+def RandomNumber():
+    return None
+def CheckUserChoice(number, guess):
+    return None
+
+main()
